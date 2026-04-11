1879a4a375d6587df78463684b29b61389260164
Deniz Alp <rdenizalp@outlook.com>
2026-04-11 20:55:03 +0300

    Initial Commit

---
+++ FilmBase/FilmBaseApp.swift
@@ -0,0 +1,32 @@
+//
+//  FilmBaseApp.swift
+//  FilmBase
+//
+//  Created by Deniz Alp on 11.04.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FilmBaseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
